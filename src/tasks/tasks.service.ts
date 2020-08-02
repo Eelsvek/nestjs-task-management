78e188bd69192739f61d413e95be74875da7c374
Kevin Lee <kev.sunlee@gmail.com>
2020-08-01 20:48:07 -0700

    filter tasks by query params

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,25 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter(task => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter(
+        task =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+
+    return tasks;
   }
 
   getTaskById(id: string): Task {
@@ -22,8 +42,8 @@ export class TasksService {
       id: uuid(),
       title,
       description,
-      status: TaskStatus.OPEN
-    }
+      status: TaskStatus.OPEN,
+    };
 
     this.tasks.push(task);
 
@@ -31,7 +51,7 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id)
+    this.tasks = this.tasks.filter(task => task.id !== id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
