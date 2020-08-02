6dfa1c77c7ed6a1617ccf53948bdd4ffe0789c7a
Kevin Lee <kev.sunlee@gmail.com>
2020-08-01 22:02:10 -0700

    TaskStatusValidationPipe

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -35,7 +35,7 @@ export class TasksService {
     const found = this.tasks.find(task => task.id === id);
 
     if (!found) {
-      throw new NotFoundException();
+      throw new NotFoundException(`Task with id ${id} not found.`);
     }
 
     return found;
@@ -57,7 +57,9 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    const found = this.getTaskById(id);
+
+    this.tasks = this.tasks.filter(task => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
